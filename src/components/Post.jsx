f47b5425dddbbe04fcc04903cdc8ff9b2ac53d85
Dino Nguyen and Kimberly McConnell <dino@>
2019-04-30 15:42:21 -0700

    Add time to post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,20 @@
-import React from 'React';
+import React, { useState } from 'react';
 import Vote from './Vote';
 import PropTypes from 'prop-types';
+import Moment from 'moment';
+
 
 function Post(props) {
+  const [time, setTime] = useState(props.timeOpen);
+  console.log(time)
   console.log("post", props)
+  
   return (
     <div>
+      <button onClick={() => setTime(Date.now() - props.timeOpen)}>Set Time</button>
+      <p>{time}</p>
       <h2>A Post {props.title}</h2>
+      <p>{props.time}</p>
       <Vote/>
     </div>
   );
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Post from './Post';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Moment from 'moment';
+
 
 function PostList(props) {
   console.log("list", props.masterPostList)
 
+  // const [time, setTime] = useState(props.timeOpen);
+
+  Object.keys(props.masterPostList).forEach((post) => 
+  
+    props.masterPostList[post].formattedTime = (props.masterPostList[post].timeOpen).fromNow(true)
+  );
+
   return(
     <div>
+      {/* <button onClick={() => setTime(time.fromNow(true))}>Set Time</button> */}
+      {/* <p>{time}</p> */}
       {Object.keys(props.masterPostList).map((postId, index) => {
         let post = props.masterPostList[postId];
         return <Post 
         title = {post.title}
         text = {post.text} 
-        timeOpen = {post.timeOpen} 
+        timeOpen = {post.formattedTime}
         id = {post.id}
         key = {index} />
         }
